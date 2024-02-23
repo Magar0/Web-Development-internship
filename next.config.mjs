db30b25b7979ff4212c60850bb2ec2a670f3f6cb
Thapa0 <rakeshmagarr0@gmail.com>
2024-02-23 12:53:24 +0530

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ["img.freepik.com", "hips.hearstapps.com", "media.istockphoto.com", "www.shutterstock.com", "t4.ftcdn.net"]
+    }
+};
 
 export default nextConfig;
---
+++ src/app/components/card/page.js
@@ -0,0 +1,18 @@
+import Image from "next/image"
+import './card.css'
+import Link from "next/link"
+const Card = ({ item }) => {
+    return (
+        <Link href={'/'} className="card w-max hover:bg-green-500 rounded-lg overflow-hidden hover:text-white text-stone-500 ease-in duration-200 ">
+            <div className="image">
+                <Image src={item.image} className="image" height={250} width={250} alt="pic" />
+            </div>
+            <div className="text-center pt-3 pb-1">
+                <h2 className="capitalize text-xl">{item.name}</h2>
+                <p className="capitalize font-light  ">{item.type}</p>
+            </div>
+        </Link>
+    )
+}
+
+export default Card
